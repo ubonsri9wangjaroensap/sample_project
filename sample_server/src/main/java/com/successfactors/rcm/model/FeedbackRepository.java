6aac99829f317c71b51015a2036a69bacf747a4b
Justin Chung <justin.chung@sap.com>
2020-10-08 20:20:04 +0200

    Remove Help and Feedback controllers

--- sample_server/src/main/java/com/successfactors/rcm/model/FeedbackRepository.java
+++
@@ -1,6 +0,0 @@
-package com.successfactors.rcm.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FeedbackRepository extends CrudRepository<Feedback, Integer> {
-}
