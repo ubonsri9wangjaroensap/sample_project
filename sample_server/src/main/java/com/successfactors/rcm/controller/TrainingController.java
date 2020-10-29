7bcd42b9d5f772ac7b439bc6daa381d35ff4a897
Justin Chung <justin.chung@sap.com>
2020-10-29 21:50:27 +0100

    Clean up TrainingController

--- sample_server/src/main/java/com/successfactors/rcm/controller/TrainingController.java
+++ sample_server/src/main/java/com/successfactors/rcm/controller/TrainingController.java
@@ -1,7 +1,6 @@
 package com.successfactors.rcm.controller;
 
 import org.json.*;
-import com.successfactors.rcm.dto.feedback.TrainRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,20 +21,13 @@ public class TrainingController {
 
     @PostMapping
     public ResponseEntity train(@RequestBody String request) {
-//        if (request.getKey() != null) {
-//            jedis.hset(request.getKey(), RESPONSE, request.getResponse());
-//            jedis.hset(request.getKey(), TYPE, request.getType());
-//        }
-//
-//        System.out.println(jedis.hget(request.getKey(), RESPONSE));
-//        System.out.println(jedis.hget(request.getKey(), TYPE));
         try {
             JSONObject jsonObject = new JSONObject(request);
             System.out.println(jsonObject.getString("key"));
             JSONObject jsonObj = jsonObject.getJSONObject("response");
             jedis.set(jsonObject.getString("key"),jsonObj.toString());
             System.out.println(jsonObj.toString());
-        }catch (JSONException err){
+        } catch (JSONException err){
             System.out.println("Error");
         }
 
